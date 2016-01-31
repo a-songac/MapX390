f5846dcdb556318871bbeb815647ea6fae44086a
Arnaud Songa-Côté <songa11@hotmail.com>
2016-01-31 18:42:04 -0500

    Load into webview Leaflet maps MAPX390-22

--- app/src/main/java/soen390/mapx/ui/view/binder/MapFragmentViewBinder.java
+++ app/src/main/java/soen390/mapx/ui/view/binder/MapFragmentViewBinder.java
@@ -1,5 +1,8 @@
 package soen390.mapx.ui.view.binder;
 
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.arnaud.android.core.ui.BaseViewBinder;
 import com.arnaud.android.core.ui.BaseViewHolder;
 
@@ -11,21 +14,26 @@ import soen390.mapx.ui.view.holder.MapFragmentViewHolder;
 public class MapFragmentViewBinder extends BaseViewBinder {
 
     private MapFragmentViewHolder viewHolder;
+    private WebView webView;
 
     public MapFragmentViewBinder(BaseViewHolder viewHolder) {
         super(viewHolder);
 
         this.viewHolder = (MapFragmentViewHolder) viewHolder;
+        webView = this.viewHolder.getWebView();
 
     }
 
     /**
-     * Bind data to view
+     * Bind Leaflet maps to WebView
      */
+    @SuppressWarnings("setJavascriptEnabled")
     public void bind() {
 
-        // TODO Bind content to webView
-        viewHolder.getWebView();
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/map_webview/index.html");
+
 
     }
 }
