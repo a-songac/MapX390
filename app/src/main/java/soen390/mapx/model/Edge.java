3098d6f9e3e73f3addcfd4b9b13273c2001bba68
Arnaud Songa-Côté <songa11@hotmail.com>
2016-02-03 13:49:10 -0500

    Add sugar ORM framework for persistance, and create POI and Edge entities MAPX390-23

---
+++ app/src/main/java/soen390/mapx/model/Edge.java
@@ -0,0 +1,13 @@
+package soen390.mapx.model;
+
+import com.orm.SugarRecord;
+
+/**
+ * Edges model
+ */
+public class Edge extends SugarRecord{
+
+    private long p1Id;
+    private long p2Id;
+
+}
---
+++ app/src/main/java/soen390/mapx/model/POI.java
@@ -0,0 +1,16 @@
+package soen390.mapx.model;
+
+import com.orm.SugarRecord;
+
+/**
+ * Point of interest model
+ */
+
+public class POI extends SugarRecord {
+
+    private String name;
+    private int xCoord;
+    private int yCoord;
+
+
+}
