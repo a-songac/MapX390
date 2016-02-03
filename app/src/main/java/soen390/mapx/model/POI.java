bce4a51e5831d5ea9a820b2a040965218b9a1b79
Arnaud Songa-Côté <songa11@hotmail.com>
2016-02-03 14:14:11 -0500

    Complete POI model MAPX390-22

--- app/src/main/java/soen390/mapx/model/POI.java
+++ app/src/main/java/soen390/mapx/model/POI.java
@@ -3,7 +3,7 @@ package soen390.mapx.model;
 import com.orm.SugarRecord;
 
 /**
- * Point of interest model
+ * Point of interest model.
  */
 
 public class POI extends SugarRecord {
@@ -11,6 +11,18 @@ public class POI extends SugarRecord {
     private String name;
     private int xCoord;
     private int yCoord;
+
+    /**
+     * Discriminant for type of the POI between Transition point, Service point or Exhibition
+     */
+    private char type;
+
+    /**
+     * Subtype relevant to transition (i (intersection), s (stairs), e (elevator))
+     * and to Service point (w (washrooms), i (information service))
+     * Null otherwise
+     */
+    private char subType;
 
 
 }
