d3e989a41a2cb84690f720f4bbd33487b1181877
Eric Lam <ericlam51@gmail.com>
2016-02-05 13:56:04 -0500

    Add test to test Floor JSON generator

--- app/src/test/java/soen390/mapx/webapp/POIJSInterfaceTest.java
+++ app/src/test/java/soen390/mapx/webapp/POIJSInterfaceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import soen390.mapx.model.Floor;
 import soen390.mapx.model.POI;
 
 import static org.junit.Assert.*;
@@ -17,11 +18,12 @@ public class POIJSInterfaceTest {
 
     private POIJSInterface poi_int;
     Context context = new MockContext();
-    List<POI> list;
+    List<POI> list_poi;
+    List<Floor> list_floor;
 
     @Before
     public void initialize() {
-        list = new ArrayList<POI>();
+        list_poi = new ArrayList<POI>();
         poi_int = new POIJSInterface(context);
         POI poi = new POI();
         poi.setId((long) 1);
@@ -30,12 +32,25 @@ public class POIJSInterfaceTest {
         poi.setFloorId((long) 1);
         poi.setxCoord(75);
         poi.setyCoord(100);
-        list.add(poi);
+        list_poi.add(poi);
+
+        list_floor = new ArrayList<Floor>();
+        Floor floor = new Floor();
+        floor.setId((long) 1);
+        floor.setFloorNum(1);
+        floor.setImageFilePath("tiles/floor_1.jpg");
+        floor.setImageHeight(700);
+        floor.setImageWidth(1800);
+        list_floor.add(floor);
     }
 
     @Test
     public void testgetPOIsJSON(){
-        assertEquals("{\"poi\":[{\"x_coord\":75,\"y_coord\":100,\"_id\":1,\"title\":\"POI_1\",\"type\":\"e\",\"floor\":1}]}", poi_int.buildPOIJSON(list));
+        assertEquals("{\"poi\":[{\"x_coord\":75,\"y_coord\":100,\"_id\":1,\"title\":\"POI_1\",\"type\":\"e\",\"floor\":1}]}", poi_int.buildPOIJSON(list_poi));
+    }
 
+    @Test
+    public void testgetFloorJSON(){
+        assertEquals("{\"floor\":[{\"floor_num\":1,\"floor_path\":\"tiles/floor_1.jpg\",\"floor_width\":1800,\"floor_height\":700}]}", poi_int.buildFloorJSON(list_floor));
     }
 }
