76363bf02c8cd6e0acdf28352c42d608b970f7e8
bosniak47 <bosniak.bos@gmail.com>
2016-02-18 11:33:21 -0500

    -Added a few more Junit test for the PathFinder

--- app/src/test/java/soen390/mapx/webapp/PathFinderTest.java
+++ app/src/test/java/soen390/mapx/webapp/PathFinderTest.java
@@ -15,12 +15,11 @@ public class PathFinderTest {
 //2. add the edges
 //3. compute the shortest path with pathfinder
 //4. call the shortestpath
-//4. check the answer
+//5. check the answer
 
     private int[][] edges;
     private int size;
     private WeightedGraph weightedGraph;
-    private PathFinder pathFinder;
 
     @Before
     public void initialize() {
@@ -58,33 +57,79 @@ public class PathFinderTest {
         weightedGraph.addEdge(6,1,4);
 
 
-        pathFinder = new PathFinder();
-
-
     }
 
     @Test
     public void getWeightIsNotNull(){
         int weight = weightedGraph.getWeight(0,7);
-                 Assert.assertEquals(4, weight);
+        Assert.assertEquals(4, weight);
     }
 
     @Test
     public void computeShortestPathIsNotNull(){
         int[] arrayPath;
-        //int i=0;
-        ArrayList<Integer> as = new ArrayList<Integer>();
+
+        ArrayList<Integer> expectedShortestPath = new ArrayList<Integer>();
         arrayPath = PathFinder.computeShortestPath(weightedGraph,0);
-//        for(i=0; i<arrayPath.length; i++){
-//            System.out.println("index: " + i + " " + arrayPath[i]);
-//        }
-        as.add(0,0);
-        as.add(1,3);
-        as.add(2,4);
 
-       Assert.assertEquals(as, pathFinder.getShortestPath(arrayPath, 0, 4));
+        expectedShortestPath.add(0, 0);
+        expectedShortestPath.add(1, 3);
+        expectedShortestPath.add(2, 4);
 
-
+        Assert.assertEquals(expectedShortestPath, PathFinder.getShortestPath(arrayPath, 0, 4));
     }
+
+    @Test
+    public void computeShortestPathToItSelf(){
+        int[] arrayPath;
+
+        ArrayList<Integer> expectedShortestPath = new ArrayList<Integer>();
+        arrayPath = PathFinder.computeShortestPath(weightedGraph,2);
+
+        expectedShortestPath.add(0, 2);
+
+        Assert.assertEquals(expectedShortestPath, PathFinder.getShortestPath(arrayPath, 2, 2));
+    }
+
+    @Test
+    public void computeShortestPathBackward(){
+        int[] arrayPathF;
+        int[] arrayPathB;
+
+        ArrayList<Integer> expectedShortestPathForward = new ArrayList<Integer>();
+        ArrayList<Integer> expectedShortestPathBackward = new ArrayList<Integer>();
+        arrayPathF = PathFinder.computeShortestPath(weightedGraph,5);
+        arrayPathB = PathFinder.computeShortestPath(weightedGraph,0);
+
+        expectedShortestPathForward.add(0, 5);
+        expectedShortestPathForward.add(1, 4);
+        expectedShortestPathForward.add(2, 3);
+        expectedShortestPathForward.add(3, 0);
+
+        expectedShortestPathBackward.add(0, 0);
+        expectedShortestPathBackward.add(1, 3);
+        expectedShortestPathBackward.add(2, 4);
+        expectedShortestPathBackward.add(3, 5);
+
+
+        Assert.assertEquals(expectedShortestPathForward, PathFinder.getShortestPath(arrayPathF, 5, 0));
+        Assert.assertEquals(expectedShortestPathBackward, PathFinder.getShortestPath(arrayPathB, 0, 5));
+    }
+
+    @Test
+    public void computeShortestPathWithTwoSolution(){
+        int[] arrayPath;
+
+        ArrayList<Integer> expectedShortestPath = new ArrayList<Integer>();
+        arrayPath = PathFinder.computeShortestPath(weightedGraph,7);
+
+        expectedShortestPath.add(0, 7);
+        expectedShortestPath.add(1, 0);
+        expectedShortestPath.add(2, 3);
+        expectedShortestPath.add(3, 4);
+
+        Assert.assertEquals(expectedShortestPath, PathFinder.getShortestPath(arrayPath, 7, 4));
+    }
+
 
 }
