8391b7e43a987065c6913aa7121fd2b96a7b2788
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-02-20 13:17:31 -0500

    Recommiting files LanguagesUITest and ToggleUITest because of missing code.

--- app/src/androidTest/java/soen390/mapx/ToggleUITest.java
+++ app/src/androidTest/java/soen390/mapx/ToggleUITest.java
@@ -1,7 +1,38 @@
 package soen390.mapx;
 
-/**
- * Created by Samer on 2016-02-20.
- */
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import soen390.mapx.activity.MainActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+
 public class ToggleUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    public String getResourceString(int resID){
+        return getInstrumentation().getTargetContext().getString(resID);
+    }
+
+    @Test
+    public void toglleApplicationTest() {
+        onView(withId(R.id.toolbar)).perform(click());
+        onView(withContentDescription(getResourceString(R.string.navigation_drawer_open))).perform(click());
+    }
+
 }
