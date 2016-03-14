ece77f1ef6483bc2e81466aaae63bc6ed25f715a
Carson Huang <Carson@Carsons-MacBook-Pro.local>
2016-03-14 13:35:19 -0400

    Added null exception handler
    MAPX390-58
    
    Currently, there is no logic for storyline. Therefore, path is null
    when webview requests it.
    
    In production, this should not happen. But to make the javascript work
    (ie: hide the buttons), we need to add the check, as null makes the
    javascript stop.

--- app/src/main/java/soen390/mapx/webapp/NodeJSInterface.java
+++ app/src/main/java/soen390/mapx/webapp/NodeJSInterface.java
@@ -198,13 +198,20 @@ public class NodeJSInterface {
      */
     @JavascriptInterface
     public String getPath() {
-        JSONArray pathArr = new JSONArray();
+        try{
+            JSONArray pathArr = new JSONArray();
 
-        for(int nodeID : MapManager.getCurrentPath()){
-            pathArr.put(nodeID);
+            for(int nodeID : MapManager.getCurrentPath()){
+                pathArr.put(nodeID);
+            }
+
+            return pathArr.toString();
         }
 
-        return pathArr.toString();
+        catch(Exception e){
+            //Maybe add error call?
+            return null;
+        }
     }
 
     /**
