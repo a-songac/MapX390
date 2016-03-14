8b400298d5657df6656b937a1676ae56f43593b0
Eric Lam <ericlam51@gmail.com>
2016-03-13 22:49:56 -0400

    Media player now has action bar TODO: Static on top of video MAPX390-66

--- app/src/main/java/soen390/mapx/activity/MediaPlayerActivity.java
+++ app/src/main/java/soen390/mapx/activity/MediaPlayerActivity.java
@@ -3,10 +3,13 @@ package soen390.mapx.activity;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 
@@ -17,7 +20,7 @@ import soen390.mapx.helper.ConstantsHelper;
 /**
  * Created by ericl on 3/13/2016.
  */
-public class MediaPlayerActivity extends Activity{
+public class MediaPlayerActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,24 +31,47 @@ public class MediaPlayerActivity extends Activity{
         Bundle extras = getIntent().getExtras();
         if (null != extras) {
             int position = extras.getInt(ConstantsHelper.POI_MEDIA_START_POSITION_INTENT_EXTRA_KEY);
-
-            VideoView videoView =(VideoView)findViewById(R.id.videoView);
             Uri uri = null;
+            VideoView videoView = null;
 
+            try {
+                JSONObject media = DummyData.dummyMedia().getJSONObject(position);
+                videoView = (VideoView) findViewById(R.id.videoView);
 
-            uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.moeb_point5);
+                uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.moeb_point5);
 
+                /* USE THIS WHEN ALL VIDEO ARE CONVERTED TO MP4
+                uri = Uri.parse("android.resource://" + getPackageName() +
+                        "/R.raw." + DummyData.dummyMedia().getJSONObject(position).getString("path"));
+                */
+
+                setActionBar(media.getString("title"));
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
 
             MediaController mediaController= new MediaController(this);
             mediaController.setAnchorView(videoView);
+
             videoView.setMediaController(mediaController);
             videoView.setVideoURI(uri);
             videoView.requestFocus();
 
-
             videoView.start();
-
         }
 
+    }
+
+    /**
+     * Set action bar title
+     */
+    private void setActionBar(String title) {
+
+        ActionBar actionBar = getSupportActionBar();
+        if (null != actionBar){
+            actionBar.setDisplayHomeAsUpEnabled(true);//TODO investigate duplication of the screen when setting the back navigation
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle(title);
+        }
     }
 }
