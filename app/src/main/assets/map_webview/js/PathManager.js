212fab2ad2016a6546a6b51635dc0474a431c3ba
Carson Huang <Carson@Carsons-MacBook-Pro.local>
2016-03-16 11:04:28 -0400

    Update Path logic

--- app/src/main/assets/map_webview/js/PathManager.js
+++ app/src/main/assets/map_webview/js/PathManager.js
@@ -1,6 +1,7 @@
 function PathManager(){
 	var polylines = [];
 	var sourcePoiId, destinationPoiId;
+	var path = [];
 
 	this.drawPath = function(){
 		var currentFloor = controller.floorManager.getCurrentFloor();
@@ -10,14 +11,16 @@ function PathManager(){
 		var mapWidth = controller.mapWidth;
 		var mapHeight = controller.mapHeight;
 
-		var pathJSON = Android.getPath();
+		if(path.length === 0){
+			var pathJSON = Android.getPath();
 
-		if(!pathJSON){
-			console.log("Error in function: startNavigation \nVariable: path \nMessage: Path is either null or has a length of 0");
-			return;
+			if(!pathJSON){
+				console.log("Error in function: startNavigation \nVariable: path \nMessage: Path is either null or has a length of 0");
+				return;
+			}
+
+			path = JSON.parse(pathJSON);
 		}
-
-		var path = JSON.parse(pathJSON);
 
 		var pastNode = null;
 		for(var i in path){
@@ -61,9 +64,35 @@ function PathManager(){
 			map.removeLayer(polylines[i]);
 		}
 
+		path = [];
 		polylines = [];
 		sourcePoiId = null;
 		destinationPoiId = null;
+	};
+
+	this.updatePath = function(){
+		var userMarkerLatlng = controller.userManager.getUserMarker().getLatLng();
+
+		var userPOI = Android.getUserPosition();
+		for(var i = 0; i < path.length; i++){
+			if(parseInt(userPOI) === parseInt(path[i])){
+				break;
+			}
+
+			path.shift();
+		}
+
+		while(polylines.length !== 0){
+			var pathElement = polylines[0];
+			var latLngs = pathElement.getLatLngs();
+
+			if(parseInt(userMarkerLatlng.lng) === parseInt(latLngs[0].lng) && parseInt(userMarkerLatlng.lat) === parseInt(latLngs[0].lat)){
+				break;
+			}
+
+			map.removeLayer(polylines[0]);
+			polylines.shift();
+		}
 	};
 
 	this.getSourcePOI = function(){
